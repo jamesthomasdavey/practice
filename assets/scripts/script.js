0e1df8cfda256d267f4e812da25fc2fd5344f3b6
James Davey <james.t.davey@gmail.com>
2018-09-08 12:33:54 -0700

    practice with nesting objects and arrays

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,29 @@
-const sumArray = arr => arr.reduce((acc, value) => acc + value, 0);
+const movies = [
+  {
+    title: "In Bruges",
+    rating: 5,
+    hasWatched: true
+  },
+  {
+    title: "Frozen",
+    rating: 4.5,
+    hasWatched: false
+  },
+  {
+    title: "Max Max Fury Road",
+    rating: 5,
+    hasWatched: true
+  },
+  {
+    title: "Les Miserables",
+    rating: 3.5,
+    hasWatched: false
+  }
+];
 
-console.log(sumArray([1,2,3]))
+movies.forEach(movie => {
+  let result = "You have ";
+  result += movie.hasWatched ? "watched " : "not seen ";
+  result += `"${movie.title}" - ${movie.rating} stars.`;
+  console.log(result);
+})
