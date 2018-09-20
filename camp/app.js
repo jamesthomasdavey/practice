a35ebc50bc316bf4eac89aefc37574aefee7b207
James Thomas Davey <james.t.davey@gmail.com>
2018-09-20 20:21:15 +0000

    add mongo to site, add description to form and build working show route template

--- camp/app.js
+++ camp/app.js
@@ -1,104 +1,81 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/camp", { useNewUrlParser: true });
 
-const campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Bodega Dunes",
-    image:
-      "https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Sierra City",
-    image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Bodega Dunes",
-    image:
-      "https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Sierra City",
-    image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Bodega Dunes",
-    image:
-      "https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Sierra City",
-    image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Bodega Dunes",
-    image:
-      "https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Sierra City",
-    image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Bodega Dunes",
-    image:
-      "https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104496f7c279a1e9b3bd_340.jpg"
-  },
-  {
-    name: "Sierra City",
-    image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"
-  }
-];
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
+// Campground.create({
+//   name: "Granite Hill",
+//   image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aa6e65fcad07b9a68420c430034f84f2&auto=format&fit=crop&w=800&q=60",
+//   description: "This is a huge granite hill. No bathrooms. No water. Beautiful granite!"
+// });
+
+// INDEX
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds });
+  // get campgrounds from db
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render campgrounds retrieved from db
+      res.render("index", { campgrounds });
+    }
+  })
 });
 
+// CREATE
 app.post("/campgrounds", (req, res) => {
+  // gets properties of new campground from request
   const name = req.body.name;
   const image = req.body.image;
-  campgrounds.push({
+  const description = req.body.description;
+  
+  // saves new campground to database
+  Campground.create({
     name: name,
-    image: image
+    image: image,
+    description: description
+  }, (err, newCampground) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("campgrounds");
+
+    }
   });
-  res.redirect("campgrounds");
 });
 
+// NEW
 app.get("/campgrounds/new", (req, res) => {
   res.render("new.ejs");
 });
+
+// SHOW
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render("show", { campground });
+    }
+  })
+  // find campground with provided ID
+})
 
 app.listen(process.env.PORT, process.env.IP, () => {
   console.log("Your server is running, hue man.");
