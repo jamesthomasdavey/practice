2ebec3f029da91337e3f085f601fd7823772fbfd
James Thomas Davey <james.t.davey@gmail.com>
2018-09-23 19:40:15 +0000

    finish basic functionality with restful recipes app

--- recipes-app-practice/app.js
+++ recipes-app-practice/app.js
@@ -4,12 +4,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 // use packages
 
 mongoose.connect("mongodb://localhost:27017/recipes-app-practice", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// set public folder
+
+app.use(express.static("public"));
 
 // set up schema / model
 
@@ -23,10 +29,6 @@ const recipeSchema = new mongoose.Schema({
 })
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
-
-// set public folder
-
-app.use(express.static("public"));
 
 //// ROUTES ////
 
@@ -45,15 +47,15 @@ app.get("/recipes", (req, res) => {
     else {
       res.render("index", { pageTitle, recipes });
     }
-  })
-})
+  });
+});
 
 // new route
 
 app.get("/recipes/new", (req, res) => {
   const pageTitle = "Create a New Recipe";
   res.render("new", { pageTitle });
-})
+});
 
 app.post("/recipes", (req, res) => {
   Recipe.create(req.body.recipe, (err, addedRecipe) => {
@@ -62,6 +64,58 @@ app.post("/recipes", (req, res) => {
     }
     else {
       res.redirect("/");
+    }
+  });
+});
+
+// show route
+
+app.get("/recipes/:id", (req, res) => {
+  Recipe.findById(req.params.id, (err, recipe) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      const pageTitle = recipe.title;
+      res.render("show", { pageTitle, recipe });
+    }
+  })
+})
+
+// edit route
+
+app.get("/recipes/:id/edit", (req, res) => {
+  Recipe.findById(req.params.id, (err, recipe) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      const pageTitle = "Edit " + recipe.title;
+      res.render("edit", { pageTitle, recipe })
+    }
+  })
+})
+
+app.put("/recipes/:id", (req, res) => {
+  Recipe.findByIdAndUpdate(req.params.id, req.body.recipe, (err, recipe) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect("/recipes/" + req.params.id);
+    }
+  })
+})
+
+// delete route
+
+app.delete("/recipes/:id", (req, res) => {
+  Recipe.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect("/recipes");
     }
   })
 })
